07a4afd9c4301fd062b04090fbb89bc5915dad0c
grig-nady <146548857+grig-nady@users.noreply.github.com>
2023-10-14 16:32:29 +0300

    Wednesday_testpy

--- test2.py
+++ test2.py
@@ -10,26 +10,41 @@ from random import shuffle
 
 class Question():
     def __init__(self, question, right_answer, wrong1):
-        # все строки надо задать при создании объекта, они запоминаются в свойства
         self.question = question
         self.right_answer = right_answer
         self.wrong1 = wrong1
 
+
+
 questions_list = [] 
-questions_list.append(Question('КАК ЗОВУТ ДВОРЕЦКО СЕМЕЙКИ АДАМС?','Фрэнк','Ларч'))
+questions_list.append(Question('КАК ЗОВУТ ДВОРЕЦКОГО СЕМЕЙКИ АДАМС?','Ларч','Фрэнк'))
 questions_list.append(Question('КЕМ ЯВЛЯЕТСЯ БЬЯНКА?','Русалкой','Вампиром'))
-questions_list.append(Question('А ЧТО ЕСЛИ Я БОЛГАРКА?','далее','далее'))
-questions_list.append(Question('Подача LCL','далее','далее'))
+questions_list.append(Question('А ЧТО ЕСЛИ Я БОЛГАРКА?','Да','Нет'))
+questions_list.append(Question('ПРАВДА, ЧТО УЭНСДЕЙ НЕ МОРГАЕТ?','Да','Нет'))
+questions_list.append(Question('УЭНСДЕЙ ПИШЕТ КНИГУ?','Да','Нет'))
+questions_list.append(Question('КАК ЗОВУТ БРАТА УЭНСДЕЙ?','Ларч','Фрэнк'))
+questions_list.append(Question('КАК НАЗЫВАЕТСЯ ВТОРАЯ ГЛАВА УЭНСДЕЙ?','Одинокое число','Чудовище из Леса'))
+questions_list.append(Question('КАКАЯ КОМАНДА ВЫИГРАЛА КУБОК ПО?','Черные кошки','Золотые жуки'))
+questions_list.append(Question('КАК УЭНСДЕЙ ОТОМСТИЛА ХУЛИГАНАМ ЗА БРАТА?','Выпустила пираний в бассейн','Укусила хулигана'))
+questions_list.append(Question('КАК ЗОВУТ СОСЕДКУ ПО КОМНАТЕ УЭНСДЕЙ','Энид','Лиза'))
+questions_list.append(Question('ЭНИД НРАВИТСЯ АЯКС?','Да','Нет'))
+questions_list.append(Question('ЧТО НУЖНО БЫЛО СДЕЛАТЬ, ЧТОБЫ ОТКРЫЛАСЬ ДВЕРЬ В ПОТАЙНУЮ БИБЛИОТЕКУ?','Дважды щелкнуть','Прочитать стих'))
+questions_list.append(Question('КАК ЗОВУТ ОСНОВАТЕЛЯ ДЖЕРИКО?','Джозеф Крекстоун','Роберт Уилимс'))
+questions_list.append(Question('МОРТИША УЧИЛАСЬ В НЕВЕРМОРЕ?','Да','Нет'))
+questions_list.append(Question('МОРТИША И ДИРЕКТОР УЧИЛИСЬ ВМЕСТЕ?','Да','Нет'))
+questions_list.append(Question('КСАВЬЕ ВСТРЕЧАЛСЯ С БЬЯНКОЙ?','Да','Нет'))
+
+
 
 app = QApplication([])
 
 
-btn_OK = QPushButton('Ответить') # кнопка ответа
+btn_OK = QPushButton('Ответить') 
 btn_OK.setFont(QFont("Times New Roman", 20, QFont.Bold))
-lb_Question = QLabel('Самый сложный вопрос в мире!') # текст вопроса
+lb_Question = QLabel('Самый сложный вопрос в мире!') 
 lb_Question.setFont(QFont("Times New Roman", 20, QFont.Bold))
 
-RadioGroupBox = QGroupBox("Варианты ответов") # группа на экране для переключателей с ответами
+RadioGroupBox = QGroupBox("Варианты") 
 RadioGroupBox.setFont(QFont("Times New Roman", 20, QFont.Bold))
 
 
@@ -38,30 +53,30 @@ rbtn_2 = QRadioButton('Вариант 2')
 
 
 
-RadioGroup = QButtonGroup() # это для группировки переключателей, чтобы управлять их поведением
+RadioGroup = QButtonGroup() 
 RadioGroup.addButton(rbtn_1)
 RadioGroup.addButton(rbtn_2)
 
 
 
 layout_ans1 = QHBoxLayout()   
-layout_ans2 = QVBoxLayout() # вертикальные будут внутри горизонтального
+layout_ans2 = QVBoxLayout() 
 layout_ans3 = QVBoxLayout()
-layout_ans2.addWidget(rbtn_1) # два ответа в первый столбец
+layout_ans2.addWidget(rbtn_1)
 layout_ans2.addWidget(rbtn_2)
 
 
 
 layout_ans1.addLayout(layout_ans2)
-layout_ans1.addLayout(layout_ans3) # разместили столбцы в одной строке
+layout_ans1.addLayout(layout_ans3) 
 
 
-RadioGroupBox.setLayout(layout_ans1) # готова "панель" с вариантами ответов 
+RadioGroupBox.setLayout(layout_ans1) 
 
 
 AnsGroupBox = QGroupBox("Результат теста")
-lb_Result = QLabel('прав ты или нет?') # здесь размещается надпись "правильно" или "неправильно"
-lb_Correct = QLabel('ответ будет тут!') # здесь будет написан текст правильного ответа
+lb_Result = QLabel('прав ты или нет?') 
+lb_Correct = QLabel('ответ будет тут!')
 
 
 layout_res = QVBoxLayout()
@@ -70,19 +85,19 @@ layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter, stretch=2)
 AnsGroupBox.setLayout(layout_res)
 
 
-layout_line1 = QHBoxLayout() # вопрос
-layout_line2 = QHBoxLayout() # варианты ответов или результат теста
-layout_line3 = QHBoxLayout() # кнопка "Ответить"
+layout_line1 = QHBoxLayout() 
+layout_line2 = QHBoxLayout() 
+layout_line3 = QHBoxLayout() 
 
 
 layout_line1.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
 layout_line2.addWidget(RadioGroupBox)   
 layout_line2.addWidget(AnsGroupBox)  
-AnsGroupBox.hide() # скроем панель с ответом, сначала должна быть видна панель вопросов
+AnsGroupBox.hide() 
 
 
 layout_line3.addStretch(1)
-layout_line3.addWidget(btn_OK, stretch=2) # кнопка должна быть большой
+layout_line3.addWidget(btn_OK, stretch=2) 
 layout_line3.addStretch(1)
 
 
@@ -94,7 +109,7 @@ layout_card.addLayout(layout_line2, stretch=8)
 layout_card.addStretch(1)
 layout_card.addLayout(layout_line3, stretch=1)
 layout_card.addStretch(1)
-layout_card.setSpacing(5) # пробелы между содержимым
+layout_card.setSpacing(5) 
 
 
 def show_result():
@@ -109,75 +124,60 @@ def show_question():
     RadioGroupBox.show()
     AnsGroupBox.hide()
     btn_OK.setText('Ответить')
-    # сбросить выбранную радио-кнопку
-    RadioGroup.setExclusive(False) # сняли ограничения, чтобы можно было сбросить выбор радиокнопки
+    RadioGroup.setExclusive(False)
     rbtn_1.setChecked(False)
     rbtn_2.setChecked(False)
-    RadioGroup.setExclusive(True) # вернули ограничения, теперь только одна радиокнопка может быть выбрана
-
+    RadioGroup.setExclusive(True) 
 
 answers = [rbtn_1, rbtn_2]
 
 
 def ask(q: Question):
-    ''' функция записывает значения вопроса и ответов в соответствующие виджеты, 
-    при этом варианты ответов распределяются случайным образом'''
-    shuffle(answers) # перемешали список из кнопок, теперь на первом месте списка какая-то непредсказуемая кнопка
-    answers[0].setText(q.right_answer) # первый элемент списка заполним правильным ответом, остальные - неверными
+    shuffle(answers) 
+    answers[0].setText(q.right_answer)
     answers[1].setText(q.wrong1)
 
-    lb_Question.setText(q.question) # вопрос
-    lb_Correct.setText(q.right_answer) # ответ 
-    show_question() # показываем панель вопросов 
+    lb_Question.setText(q.question) 
+    lb_Correct.setText(q.right_answer) 
+    show_question() 
 
 
 def show_correct(res):
-    ''' показать результат - установим переданный текст в надпись "результат" и покажем нужную панель '''
     lb_Result.setText(res)
     show_result()
 
 
 def check_answer():
-    ''' если выбран какой-то вариант ответа, то надо проверить и показать панель ответов'''
     if answers[0].isChecked() or answers[1].isChecked():
-        # правильный ответ!
         show_correct('Жми далее')
 
 
 
 def next_question():
-    ''' задает следующий вопрос из списка '''
-    # этой функции нужна переменная, в которой будет указываться номер текущего вопроса
-    # эту переменную можно сделать глобальной, либо же сделать свойством "глобального объекта" (app или window)
-    # мы заведем (ниже) свойство window.cur_question.
-    window.cur_question = window.cur_question + 1 # переходим к следующему вопросу
+    window.cur_question = window.cur_question + 1 
     if window.cur_question >= len(questions_list):
-        window.cur_question = 0 # если список вопросов закончился - идем сначала
-    q = questions_list[window.cur_question] # взяли вопрос
-    ask(q) # спросили
+        window.cur_question = 0
+    q = questions_list[window.cur_question]
+    ask(q) 
 
 
 def click_OK():
-    ''' определяет, надо ли показывать другой вопрос либо проверить ответ на этот '''
     if btn_OK.text() == 'Ответить':
-        check_answer() # проверка ответа
+        check_answer()
     else:
-        next_question() # следующий вопрос
+        next_question() 
 
 
 window = QWidget()
 window.setLayout(layout_card)
 window.setWindowTitle('КТО ТЫ ИЗ WEDNESDAY')
-# текущий вопрос из списка сделаем свойством объекта "окно", так мы сможем спокойно менять его из функции:
-window.cur_question = -1    # по-хорошему такие переменные должны быть свойствами, 
-                            # только надо писать класс, экземпляры которого получат такие свойства, 
-                            # но python позволяет создать свойство у отдельно взятого экземпляра
+window.cur_question = -1    
 
 
-btn_OK.clicked.connect(click_OK) # по нажатии на кнопку выбираем, что конкретно происходит
+btn_OK.clicked.connect(click_OK)
 
 
-# все настроено, осталось задать вопрос и показать окно:
+
 next_question()
 window.resize(400, 300)
 window.show()
